59bd83ff034b2a383270c44976de1d987ff30213
karolinagusmao <karolinab2gusmao@gmail.com>
2024-01-19 23:21:28 -0300

    "creating js"

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+        console.log("Isso aí! você descobriu o número secreto (5)");
+}
+
+//desafios
+ //1- alert("Boas vindas ao nosso site!");
+ //2- let nome = "Lua";
+ //3- let idade = "25";
+ //4- let numeroDeVendas = "50";
+ //5- let saldoDisponivel = "1000";
+ //6- let mensagemDeErro = "Erro!";
+    //alert ("mensagemDeErro");
+ //7- let nome = prompt("Qual o nome de usuário?");   
+ //8- let idade = prompt("Qual é sua idade?");
+ //9- alert("Erro!Preencha todos os campos");
+ //10- let idade = prompt("Digite sua idade:");
+        //if (idade >= 18){ 
+            //alert("Pode tirar habilitação");}
